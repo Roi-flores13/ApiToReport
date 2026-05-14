82b10ea12096fa6d0780006dd51a2c6022eebe38
Roi-flores13 <roi.flores@iteso.mx>
2026-05-14 15:14:39 -0600

    como borro esto?

--- src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) throws Exception {
-        ApiRequest a1 = new ApiRequest("123");
-        a1.standings("0", "2025-2026");
-
-
-    }
-}
